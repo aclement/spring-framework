c6b811c76a45d03fc7aa792a7f21c97071a0ecc5
Andy Clement <aclement@pivotal.io>
2018-07-16 21:32:40 -0700

    Change to initialize on first call that requires it and optimize registrationg of converters

--- spring-core/src/main/java/org/springframework/core/convert/support/DefaultConversionService.java
+++ spring-core/src/main/java/org/springframework/core/convert/support/DefaultConversionService.java
@@ -17,21 +17,29 @@
 package org.springframework.core.convert.support;
 
 import java.nio.charset.Charset;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
 import java.util.Currency;
 import java.util.Locale;
+import java.util.Properties;
+import java.util.TimeZone;
 import java.util.UUID;
 
 import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.core.convert.converter.ConverterRegistry;
+import org.springframework.core.convert.converter.GenericConverter;
 import org.springframework.lang.Nullable;
 
 /**
- * A specialization of {@link GenericConversionService} configured by default
- * with converters appropriate for most environments.
+ * A specialization of {@link GenericConversionService} configured by default with
+ * converters appropriate for most environments.
  *
- * <p>Designed for direct instantiation but also exposes the static
- * {@link #addDefaultConverters(ConverterRegistry)} utility method for ad-hoc
- * use against any {@code ConverterRegistry} instance.
+ * <p>
+ * Designed for direct instantiation but also exposes the static
+ * {@link #addDefaultConverters(ConverterRegistry)} utility method for ad-hoc use against
+ * any {@code ConverterRegistry} instance.
  *
  * @author Chris Beams
  * @author Juergen Hoeller
@@ -43,24 +51,83 @@ public class DefaultConversionService extends GenericConversionService {
 	@Nullable
 	private static volatile DefaultConversionService sharedInstance;
 
+	private boolean initialized = false;
 
 	/**
 	 * Create a new {@code DefaultConversionService} with the set of
-	 * {@linkplain DefaultConversionService#addDefaultConverters(ConverterRegistry) default converters}.
+	 * {@linkplain DefaultConversionService#addDefaultConverters(ConverterRegistry)
+	 * default converters}.
 	 */
 	public DefaultConversionService() {
-		addDefaultConverters(this);
 	}
 
+	@Override
+	public <T> T convert(Object source, Class<T> targetType) {
+		ensureInitialized();
+		return super.convert(source, targetType);
+	}
+
+	@Override
+	protected GenericConverter getConverter(TypeDescriptor sourceType,
+			TypeDescriptor targetType) {
+		ensureInitialized();
+		return super.getConverter(sourceType, targetType);
+	}
+
+	@Override
+	public Object convert(Object source, TypeDescriptor sourceType,
+			TypeDescriptor targetType) {
+		ensureInitialized();
+		return super.convert(source, sourceType, targetType);
+	}
+
+	@Override
+	public Object convert(Object source, TypeDescriptor targetType) {
+		ensureInitialized();
+		return super.convert(source, targetType);
+	}
+
+	@Override
+	public boolean canConvert(Class<?> sourceType, Class<?> targetType) {
+		ensureInitialized();
+		return super.canConvert(sourceType, targetType);
+	}
+
+	@Override
+	public boolean canConvert(TypeDescriptor sourceType, TypeDescriptor targetType) {
+		ensureInitialized();
+		return super.canConvert(sourceType, targetType);
+	}
+
+	@Override
+	public void removeConvertible(Class<?> sourceType, Class<?> targetType) {
+		ensureInitialized();
+		super.removeConvertible(sourceType, targetType);
+	}
+
+	@Override
+	public boolean canBypassConvert(TypeDescriptor sourceType,
+			TypeDescriptor targetType) {
+		ensureInitialized();
+		return super.canBypassConvert(sourceType, targetType);
+	}
+
+	private void ensureInitialized() {
+		if (!initialized) {
+			addDefaultConverters(this);
+			initialized = true;
+		}
+	}
 
 	/**
-	 * Return a shared default {@code ConversionService} instance,
-	 * lazily building it once needed.
-	 * <p><b>NOTE:</b> We highly recommend constructing individual
-	 * {@code ConversionService} instances for customization purposes.
-	 * This accessor is only meant as a fallback for code paths which
-	 * need simple type coercion but cannot access a longer-lived
+	 * Return a shared default {@code ConversionService} instance, lazily building it once
+	 * needed.
+	 * <p>
+	 * <b>NOTE:</b> We highly recommend constructing individual {@code ConversionService}
+	 * instances for customization purposes. This accessor is only meant as a fallback for
+	 * code paths which need simple type coercion but cannot access a longer-lived
 	 * {@code ConversionService} instance any other way.
+	 * 
 	 * @return the shared {@code ConversionService} instance (never {@code null})
 	 * @since 4.3.5
 	 */
@@ -80,30 +147,42 @@ public class DefaultConversionService extends GenericConversionService {
 
 	/**
 	 * Add converters appropriate for most environments.
-	 * @param converterRegistry the registry of converters to add to
-	 * (must also be castable to ConversionService, e.g. being a {@link ConfigurableConversionService})
-	 * @throws ClassCastException if the given ConverterRegistry could not be cast to a ConversionService
+	 * 
+	 * @param converterRegistry the registry of converters to add to (must also be
+	 *        castable to ConversionService, e.g. being a
+	 *        {@link ConfigurableConversionService})
+	 * @throws ClassCastException if the given ConverterRegistry could not be cast to a
+	 *         ConversionService
 	 */
 	public static void addDefaultConverters(ConverterRegistry converterRegistry) {
 		addScalarConverters(converterRegistry);
 		addCollectionConverters(converterRegistry);
 
-		converterRegistry.addConverter(new ByteBufferConverter((ConversionService) converterRegistry));
-		converterRegistry.addConverter(new StringToTimeZoneConverter());
-		converterRegistry.addConverter(new ZoneIdToTimeZoneConverter());
-		converterRegistry.addConverter(new ZonedDateTimeToCalendarConverter());
+		converterRegistry.addConverter(
+				new ByteBufferConverter((ConversionService) converterRegistry));
+		converterRegistry.addConverter(String.class, TimeZone.class,
+				new StringToTimeZoneConverter());
+		converterRegistry.addConverter(ZoneId.class, TimeZone.class,
+				new ZoneIdToTimeZoneConverter());
+		converterRegistry.addConverter(ZonedDateTime.class, Calendar.class,
+				new ZonedDateTimeToCalendarConverter());
 
 		converterRegistry.addConverter(new ObjectToObjectConverter());
-		converterRegistry.addConverter(new IdToEntityConverter((ConversionService) converterRegistry));
+		converterRegistry.addConverter(
+				new IdToEntityConverter((ConversionService) converterRegistry));
 		converterRegistry.addConverter(new FallbackObjectToStringConverter());
-		converterRegistry.addConverter(new ObjectToOptionalConverter((ConversionService) converterRegistry));
+		converterRegistry.addConverter(
+				new ObjectToOptionalConverter((ConversionService) converterRegistry));
 	}
 
 	/**
 	 * Add common collection converters.
-	 * @param converterRegistry the registry of converters to add to
-	 * (must also be castable to ConversionService, e.g. being a {@link ConfigurableConversionService})
-	 * @throws ClassCastException if the given ConverterRegistry could not be cast to a ConversionService
+	 * 
+	 * @param converterRegistry the registry of converters to add to (must also be
+	 *        castable to ConversionService, e.g. being a
+	 *        {@link ConfigurableConversionService})
+	 * @throws ClassCastException if the given ConverterRegistry could not be cast to a
+	 *         ConversionService
 	 * @since 4.2.3
 	 */
 	public static void addCollectionConverters(ConverterRegistry converterRegistry) {
@@ -113,7 +192,8 @@ public class DefaultConversionService extends GenericConversionService {
 		converterRegistry.addConverter(new CollectionToArrayConverter(conversionService));
 
 		converterRegistry.addConverter(new ArrayToArrayConverter(conversionService));
-		converterRegistry.addConverter(new CollectionToCollectionConverter(conversionService));
+		converterRegistry.addConverter(
+				new CollectionToCollectionConverter(conversionService));
 		converterRegistry.addConverter(new MapToMapConverter(conversionService));
 
 		converterRegistry.addConverter(new ArrayToStringConverter(conversionService));
@@ -122,11 +202,15 @@ public class DefaultConversionService extends GenericConversionService {
 		converterRegistry.addConverter(new ArrayToObjectConverter(conversionService));
 		converterRegistry.addConverter(new ObjectToArrayConverter(conversionService));
 
-		converterRegistry.addConverter(new CollectionToStringConverter(conversionService));
-		converterRegistry.addConverter(new StringToCollectionConverter(conversionService));
+		converterRegistry.addConverter(
+				new CollectionToStringConverter(conversionService));
+		converterRegistry.addConverter(
+				new StringToCollectionConverter(conversionService));
 
-		converterRegistry.addConverter(new CollectionToObjectConverter(conversionService));
-		converterRegistry.addConverter(new ObjectToCollectionConverter(conversionService));
+		converterRegistry.addConverter(
+				new CollectionToObjectConverter(conversionService));
+		converterRegistry.addConverter(
+				new ObjectToCollectionConverter(conversionService));
 
 		converterRegistry.addConverter(new StreamConverter(conversionService));
 	}
@@ -134,38 +218,50 @@ public class DefaultConversionService extends GenericConversionService {
 	private static void addScalarConverters(ConverterRegistry converterRegistry) {
 		converterRegistry.addConverterFactory(new NumberToNumberConverterFactory());
 
+		ObjectToStringConverter otsc = new ObjectToStringConverter();
 		converterRegistry.addConverterFactory(new StringToNumberConverterFactory());
-		converterRegistry.addConverter(Number.class, String.class, new ObjectToStringConverter());
+		converterRegistry.addConverter(Number.class, String.class, otsc);
 
-		converterRegistry.addConverter(new StringToCharacterConverter());
-		converterRegistry.addConverter(Character.class, String.class, new ObjectToStringConverter());
+		converterRegistry.addConverter(String.class, Character.class,
+				new StringToCharacterConverter());
+		converterRegistry.addConverter(Character.class, String.class, otsc);
 
-		converterRegistry.addConverter(new NumberToCharacterConverter());
+		converterRegistry.addConverter(Number.class, Character.class,
+				new NumberToCharacterConverter());
 		converterRegistry.addConverterFactory(new CharacterToNumberFactory());
 
-		converterRegistry.addConverter(new StringToBooleanConverter());
-		converterRegistry.addConverter(Boolean.class, String.class, new ObjectToStringConverter());
+		converterRegistry.addConverter(String.class, Boolean.class,
+				new StringToBooleanConverter());
+		converterRegistry.addConverter(Boolean.class, String.class, otsc);
 
 		converterRegistry.addConverterFactory(new StringToEnumConverterFactory());
-		converterRegistry.addConverter(new EnumToStringConverter((ConversionService) converterRegistry));
+		converterRegistry.addConverter(
+				new EnumToStringConverter((ConversionService) converterRegistry));
 
 		converterRegistry.addConverterFactory(new IntegerToEnumConverterFactory());
-		converterRegistry.addConverter(new EnumToIntegerConverter((ConversionService) converterRegistry));
+		converterRegistry.addConverter(
+				new EnumToIntegerConverter((ConversionService) converterRegistry));
 
-		converterRegistry.addConverter(new StringToLocaleConverter());
-		converterRegistry.addConverter(Locale.class, String.class, new ObjectToStringConverter());
+		converterRegistry.addConverter(String.class, Locale.class,
+				new StringToLocaleConverter());
+		converterRegistry.addConverter(Locale.class, String.class, otsc);
 
-		converterRegistry.addConverter(new StringToCharsetConverter());
-		converterRegistry.addConverter(Charset.class, String.class, new ObjectToStringConverter());
+		converterRegistry.addConverter(String.class, Charset.class,
+				new StringToCharsetConverter());
+		converterRegistry.addConverter(Charset.class, String.class, otsc);
 
-		converterRegistry.addConverter(new StringToCurrencyConverter());
-		converterRegistry.addConverter(Currency.class, String.class, new ObjectToStringConverter());
+		converterRegistry.addConverter(String.class, Currency.class,
+				new StringToCurrencyConverter());
+		converterRegistry.addConverter(Currency.class, String.class, otsc);
 
-		converterRegistry.addConverter(new StringToPropertiesConverter());
-		converterRegistry.addConverter(new PropertiesToStringConverter());
+		converterRegistry.addConverter(String.class, Properties.class,
+				new StringToPropertiesConverter());
+		converterRegistry.addConverter(Properties.class, String.class,
+				new PropertiesToStringConverter());
 
-		converterRegistry.addConverter(new StringToUUIDConverter());
-		converterRegistry.addConverter(UUID.class, String.class, new ObjectToStringConverter());
+		converterRegistry.addConverter(String.class, UUID.class,
+				new StringToUUIDConverter());
+		converterRegistry.addConverter(UUID.class, String.class, otsc);
 	}
 
 }
